19471be14f5a03e3078ce9c72266c63e8c221902
RegularGuy1998 <40391774+RegularGuy1998@users.noreply.github.com>
2019-05-21 11:44:56 +0700

    Done Post Job N time

--- Post Job/js/postjob.js
+++ Post Job/js/postjob.js
@@ -16,7 +16,51 @@ function openPostJobTab(evt, postJobName) {
     else {
         evt.currentTarget.className += " active";
     }
+}
 
+function openInfoTab(evt, postJobName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("info-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("info-tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(postJobName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+
+    if (postJobName !== 'Attached-CV') {
+        document.getElementById('itnavi-attached').className = document.getElementById('itnavi-attached').className.replace(' fullscreen', '');
+        document.getElementById('ITNavi-TitleRight').className += ' hidden';
+    } else {
+        document.getElementById('ITNavi-TitleRight').className = document.getElementById('ITNavi-TitleRight').className.replace(' hidden', '');
+    }
+}
+
+function TitleRightfull () {
+    if ($('#itnavi-attached').hasClass('fullscreen')) {
+        $('#itnavi-attached').removeClass('fullscreen');
+        document.getElementById('fullscreenbutton').innerHTML = ' <div style="background-image: url(image/full-size@3x.png)" class="button-icon"></div> View Fullscreen';
+    } else {
+        $('#itnavi-attached').addClass('fullscreen');
+        document.getElementById('fullscreenbutton').innerHTML = ' <div style="background-image: url(image/full-size@3x.png)" class="button-icon"></div> Out Fullscreen';
+    }
+    // document.getElementById('itnavi-attached').className += ' fullscreen';
+}
+
+function TitleRightfull2 () {
+    if ($('#itnavi-attached2').hasClass('fullscreen')) {
+        $('#itnavi-attached2').removeClass('fullscreen');
+        document.getElementById('fullscreenbutton2').innerHTML = ' <div style="background-image: url(image/full-size@3x.png)" class="button-icon"></div> View Fullscreen';
+    } else {
+        console.log('ko co');
+        $('#itnavi-attached2').addClass('fullscreen');
+        document.getElementById('fullscreenbutton2').innerHTML = ' <div style="background-image: url(image/full-size@3x.png)" class="button-icon"></div> Out Fullscreen';
+    }
+    // document.getElementById('itnavi-attached').className += ' fullscreen';
 }
 
 
@@ -64,6 +108,51 @@ $('.PJ-ManageJob-PostJob-button').on('click', function () {
     CallSomethingHide('ManageJobs-JobList');
 })
 
+
+function openSearchTips(evt) {
+    if ($('#search-tip-show').hasClass('hidden')) {
+        $('#search-tip-show').removeClass('hidden');
+        document.getElementById('search-tips-button').innerHTML = '<img src="image/apple@3x.png" alt=""> Hide tips';
+    } else {
+        $('#search-tip-show').addClass('hidden');
+        document.getElementById('search-tips-button').innerHTML = '<img src="image/apple@3x.png" alt=""> Search tips';
+    }
+}
+
+$('#BASICINFOMATIONButton').on('click', function () {
+    window.scrollTo(0, document.getElementById('BASICINFOMATION').offsetTop - 173);
+})
+$('#WORKENVIRONMENTButton').on('click', function () {
+    window.scrollTo(0, document.getElementById('WORKENVIRONMENT').offsetTop - 173);
+})
+$('#HEADERIMAGEPICTURESButton').on('click', function () {
+    window.scrollTo(0, document.getElementById('HEADERIMAGEPICTURES').offsetTop - 173);
+})
+
+
+window.onscroll = function () {
+    if (window.pageYOffset + 59 > defaultheader) {
+        document.getElementById('Companyprofile-Header').classList.add('headerfixtop');
+    } else {
+        document.getElementById('Companyprofile-Header').classList.remove('headerfixtop');
+    }
+
+    if (window.pageYOffset + 220 > WORKENVIRONMENT) {
+        if (window.pageYOffset + 220 > HEADERIMAGEPICTURES) {
+            document.getElementById('BASICINFOMATIONButton').classList.remove('active');
+            document.getElementById('WORKENVIRONMENTButton').classList.remove('active');
+            document.getElementById('HEADERIMAGEPICTURESButton').classList.add('active');
+        } else {
+            document.getElementById('BASICINFOMATIONButton').classList.remove('active');
+            document.getElementById('WORKENVIRONMENTButton').classList.add('active');
+            document.getElementById('HEADERIMAGEPICTURESButton').classList.remove('active');
+        }
+    } else {
+        document.getElementById('BASICINFOMATIONButton').classList.add('active');
+        document.getElementById('WORKENVIRONMENTButton').classList.remove('active');
+        document.getElementById('HEADERIMAGEPICTURESButton').classList.remove('active');
+    }
+}
 
 
 
